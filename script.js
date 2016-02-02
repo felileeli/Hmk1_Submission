4baabf9fc884480ba46307c3868fd7d530c14596
felileeli <fleetz@gmail.com>
2016-02-01 21:30:41 -0500

    First commit

---
+++ script.js
@@ -0,0 +1,11 @@
+var activities = [
+    "Exercise Yoga",
+    "Read",
+    "Doodle",
+    "Craft"
+];
+	var i;
+	for(i=0; i<activities.length;i++){
+		console.log("Activity",i, activities[i]);
+	}
+
